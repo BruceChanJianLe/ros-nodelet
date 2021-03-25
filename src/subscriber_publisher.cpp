2f9d0d458cd191a0bbd48333812ad69705f52df2
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-03-25 15:19:59 +0800

    add sub pub source file

---
+++ src/subscriber_publisher.cpp
@@ -0,0 +1,14 @@
+#include "ros-nodelet/subscriber_publisher.hpp"
+
+
+namespace nodelet_example
+{
+    subscriber_publisher::subscriber_publisher()
+    {
+    }
+
+    subscriber_publisher::~subscriber_publisher()
+    {
+    }
+
+} // namespace nodelet_example
