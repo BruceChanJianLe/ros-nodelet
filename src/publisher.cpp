506b1f939e21a842d22befeb3be4177ece1507d0
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-03-25 13:43:50 +0800

    add publisher source file

---
+++ src/publisher.cpp
@@ -0,0 +1,14 @@
+#include "ros-nodelet/publisher.hpp"
+
+
+namespace nodelet_example
+{
+    publisher::publisher()
+    {
+    }
+    
+    publisher::~publisher()
+    {
+    }
+    
+} // namespace nodelet_example
