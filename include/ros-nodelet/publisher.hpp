b76cdb5e5d29e0b9614259d4923ed5bf373e3981
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-03-25 13:44:06 +0800

    add publisher header file

---
+++ include/ros-nodelet/publisher.hpp
@@ -0,0 +1,22 @@
+#ifndef PUB_H_
+#define PUB_H_
+
+#include <ros/ros.h>
+
+#include <std_msgs/Int64.h>
+
+
+namespace nodelet_example
+{
+    class publisher
+    {
+    private:
+        ;
+    public:
+        publisher();
+        ~publisher();
+    };
+} // namespace nodelet_example
+
+
+#endif
