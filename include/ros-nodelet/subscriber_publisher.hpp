9384b561a071378256f8b8d65ba43bce46b0c2d7
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-03-25 15:19:42 +0800

    add sub pub header file

---
+++ include/ros-nodelet/subscriber_publisher.hpp
@@ -0,0 +1,22 @@
+#ifndef SUB_PUB_H_
+#define SUB_PUB_H_
+
+#include <ros/ros.h>
+
+#include <nodelet/nodelet.h>
+
+
+namespace nodelet_example
+{
+    class subscriber_publisher
+    {
+    private:
+        ;
+    public:
+        subscriber_publisher();
+        ~subscriber_publisher();
+    };
+} // namespace nodelet_example
+
+
+#endif
