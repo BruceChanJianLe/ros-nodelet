f7eb60cab3039cac2c4739b04ab0523465ea27e5
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-03-25 16:07:52 +0800

    update sub pub source file

--- src/subscriber_publisher.cpp
+++ src/subscriber_publisher.cpp
@@ -1,4 +1,5 @@
 #include "ros-nodelet/subscriber_publisher.hpp"
+#include <pluginlib/
 
 
 namespace nodelet_example
@@ -9,6 +10,32 @@ namespace nodelet_example
 
     subscriber_publisher::~subscriber_publisher()
     {
+    }
+
+    void subscriber_publisher::onInit()
+    {
+        // Init global and private nodehanle
+        global_nh_ = getNodeHandle();
+        private_nh_ = getPrivateNodeHandle();
+
+        sub_ = global_nh_.subscribe<std_msgs::Int64>(
+            "counts", 1,
+            [this](std_msgs::Int64::ConstPtr & msg)
+            {
+                this->CountCB(msg);
+            }
+        );
+
+        pub_ = private_nh_.advertise<std_msgs::Int64>("counts", 1, true);
+    }
+
+    void subscriber_publisher::CountCB(std_msgs::Int64::ConstPtr & msg)
+    {
+        // Pass data from msg to msg_
+        msg_.data = msg->data;
+
+        // Publish what is recieve
+        pub_.publish(msg_);
     }
 
 } // namespace nodelet_example
