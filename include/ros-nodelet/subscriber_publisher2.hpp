c1d0ff169c984a7d208a63c5298f4b2f8deeda09
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-03-26 15:54:46 +0800

    add sub pub 2 header file

---
+++ include/ros-nodelet/subscriber_publisher2.hpp
@@ -0,0 +1,32 @@
+#ifndef SUB_PUB_2_H_
+#define SUB_PUB_2_H_
+
+#include <ros/ros.h>
+
+#include <nodelet/nodelet.h>
+#include <std_msgs/Int64.h>
+
+
+namespace nodelet_example
+{
+    class subscriber_publisher2 : public nodelet::Nodelet
+    {
+    private:
+        virtual void onInit() override;
+
+        void CountCB(const std_msgs::Int64::ConstPtr & msg);
+
+        ros::NodeHandle gloabal_nh_;
+        ros::NodeHandle private_nh_;
+        ros::Subscriber sub_;
+        ros::Publisher pub_;
+        std_msgs::Int64 msg_;
+
+    public:
+        subscriber_publisher2();
+        ~subscriber_publisher2();
+    };
+} // namespace nodelet_example
+
+
+#endif
