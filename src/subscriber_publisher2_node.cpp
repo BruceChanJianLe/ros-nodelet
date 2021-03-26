f16cb6d3cf259f82b310d237ddc63e023d47e1cc
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-03-26 15:55:10 +0800

    add sub pub node source file

---
+++ src/subscriber_publisher2_node.cpp
@@ -0,0 +1,34 @@
+#include <ros/ros.h>
+
+#include <nodelet/loader.h>
+#include <string>
+
+
+std::string ROSNodeName {"nodelet_sub_pub_node2"};
+std::string NodeletName {"ros-nodelet/subscriber_publisher2"};
+
+int main(int argc, char ** argv)
+{
+    // Initialize ROS node
+    ros::init(argc, argv, ROSNodeName);
+
+    // Instantiate ROS nodelet
+    nodelet::Loader nodelet;
+
+    // Nodelet remap
+    nodelet::M_string remap(ros::names::getRemappings());
+
+    // Nodelet argv
+    nodelet::V_string nargv;
+
+    // Obtain nodename, this can be skipped and use ROSNodeName instead
+    std::string nodelet_name = ros::this_node::getName();
+
+    // Load nodelet
+    nodelet.load(nodelet_name, NodeletName, remap, nargv);
+
+    // ROS Spin
+    ros::spin();
+
+    return 0;
+}
